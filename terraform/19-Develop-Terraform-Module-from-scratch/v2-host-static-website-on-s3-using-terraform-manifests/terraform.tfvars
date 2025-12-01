f50d800e94c54374ee301d36b46abca6c8f93be5
Zen Seo <teamcmcbot@gmail.com>
2025-12-01 15:15:39 +0800

    Terraform course: Completed Section 18 develop terraform modules from scratch

---
+++ terraform/19-Develop-Terraform-Module-from-scratch/v2-host-static-website-on-s3-using-terraform-manifests/terraform.tfvars
@@ -0,0 +1,5 @@
+bucket_name = "mybucket-2051-terraform-static-website"
+tags = {
+  Terraform   = "true"
+  Environment = "dev"
+}
---
+++ terraform/19-Develop-Terraform-Module-from-scratch/v2-host-static-website-on-s3-using-terraform-manifests/versions.tf
@@ -0,0 +1,20 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.14.0" # which means any version equal & above 1.14.0
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+/*
+Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
+$HOME/.aws/credentials
+*/
